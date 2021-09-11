421e70b9b36cb88dd994ed2d821324572ad4910a
Dario <darioingannamorte@mail.ru>
2021-09-11 13:06:15 +0700

    check before review

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import UserViewSet, FollowViewSet
+from .views import FollowViewSet, UserViewSet
 
 router = DefaultRouter()
 router.register('users', UserViewSet, basename='Users')
