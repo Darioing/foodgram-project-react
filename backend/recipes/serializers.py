8a77e7c3399912213ec7d012c50db5b345ea087d
Dario <darioingannamorte@mail.ru>
2021-10-07 15:15:29 +0700

    change cycle, didnt work

--- backend/recipes/serializers.py
+++ backend/recipes/serializers.py
@@ -106,7 +106,7 @@ class RecipeIngredientSerializer(serializers.ModelSerializer):
 
 class CreateRecipeIngredientSerializer(RecipeIngredientSerializer):
 
-    id = serializers.PrimaryKeyRelatedField(queryset=Ingredient.objects.all(), many=True)
+    id = serializers.IntegerField(write_only=True)
     amount = serializers.IntegerField(write_only=True)
 
     def to_representation(self, instance):
@@ -125,6 +125,7 @@ class RecipeSerializer(serializers.ModelSerializer):
     )
     ingredients = CreateRecipeIngredientSerializer(many=True)
     image = Base64ImageField()
+    cooking_time = serializers.IntegerField()
     is_favorited = serializers.SerializerMethodField()
     is_in_shopping_cart = serializers.SerializerMethodField()
 
@@ -159,60 +160,54 @@ class RecipeSerializer(serializers.ModelSerializer):
             user=request.user, recipe=obj
         ).exists()
 
-    def validate_ingredients(self, value):
-        ingredients = self.initial_data.get('ingredients')
-        if len(ingredients) < 1:
+    def validate_cooking_time(self, value):
+        if value < 1:
             raise serializers.ValidationError(
-                'Вы должны выбрать хотя бы один ингредиент'
+                'Время приготовления не может быть меньше минуты'
             )
         return value
 
-    def validate_tags(self, data):
-        tags = self.initial_data.get('tags')
-        if len(tags) < 1:
+    def validate_tags(self, value):
+        if len(value) < 1:
             raise serializers.ValidationError(
                 'Вы должны указать хотя бы один тег'
             )
+        self.unique_validator(value)
+
+        return value
+
+    def validate_ingredients(self, value):
+        if len(value) < 1:
+            raise serializers.ValidationError(
+                'Вы должны выбрать хотя бы один ингредиент'
+            )
+        for ing in value:
+            if ing['amount'] < 1:
+                raise serializers.ValidationError(
+                    'Количество ингредиента не должно быть меньше одного'
+                )
+        return value
+
+    def unique_validator(self, value):
+        set_value = len(set(value))
+        if set_value != len(value):
+            raise serializers.ValidationError(
+                'Не должно быть повторений в поле'
+            )
+        return True
 
     def create_or_update_cycle(self, recipe, ingredients_data):
-        """
-        Добавлю небольшой комментарий, проверка на
-        отрицательный вес ингредиента выполняется полем
-        amount модели RecipeIngredient т.к. оно 
-        PositiveSmallIntegerField(выяснили в треде слака)
-        тоже самое с cooking_time => не добавлял проверку
-        """
-        ingredients_instance = RecipeIngredient.objects.values_list(
-            'id', 'amount', 
-        )
-        repeated_ingredient = []
         for ingredient in ingredients_data:
-            if ingredient not in repeated_ingredient:  # проверка на повтор
-                repeated_ingredient.append(ingredient)
-            else:
-                raise serializers.ValidationError(
-                    'Ингредиенты не должны повторяться'
-                )
-            amount = ingredient['amount']
             ingredient_id = ingredient['id']
-            ingredient_instance = get_object_or_404(
-                Ingredient, id=ingredient_id
+            amount = ingredient['amount']
+            obj, created = RecipeIngredient.objects.get_or_create(
+                ingredient=ingredient_id,
+                recipe=recipe,
+                defaults={'amount': amount}
             )
-            
-            if RecipeIngredient.objects.filter(
-                id=ingredient_id, amount=amount
-            ).exists():
-                ingredients_instance.remove(
-                    RecipeIngredient.objects.get(
-                        id=ingredient_id, amount=amount
-                    ).ingredient
-                )
-            else:
-                RecipeIngredient.objects.get_or_create(
-                    recipe=recipe,
-                    ingredient=ingredient_instance,
-                    amount=amount
-                )
+            if not created:
+                obj.amount += amount
+                obj.save()
         
 
     def create(self, validated_data):
@@ -222,28 +217,29 @@ class RecipeSerializer(serializers.ModelSerializer):
         recipe = Recipe.objects.create(
             author=request.user, **validated_data
         )
-        recipe.tags.set(tags_data)
         self.create_or_update_cycle(
-            self, recipe, ingredients_data
+            recipe, ingredients_data
         )
+        recipe.tags.set(tags_data)
         recipe.save()
         return recipe
 
     def update(self, instance, validated_data):
         ingredients_data = validated_data.pop('ingredients')
         tags_data = validated_data.pop('tags')
-        recipe = Recipe.objects.filter(id=instance.id)
-        recipe.update(**validated_data)
-        ingredients_instance = RecipeIngredient.objects.values_list(
-            'id', 'amount', 
+        instance.name = validated_data.get('name', instance.name)
+        instance.image = validated_data.get('image', instance.image)
+        instance.text = validated_data.get('text', instance.text)
+        instance.cooking_time = validated_data.get(
+            'cooking_time',
+            instance.cooking_time
         )
-        RecipeSerializer.create_or_update_cycle(self, recipe, ingredients_data)
-        if validated_data.get('image') is not None:
-            instance.image = validated_data.get(
-                'image', instance.image
-            )
-        instance.ingredients.remove(*ingredients_instance)
+        instance.ingredients.clear()
+        self.create_or_update_cycle(
+            instance, ingredients_data
+        )
         instance.tags.set(tags_data)
+        instance.save()
         return instance
 
 
